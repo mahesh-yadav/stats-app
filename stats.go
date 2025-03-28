30a0232cf086718efa63279919faac29f649a363
Mahesh Yadav <mahesh.iitmandi@gmail.com>
2025-03-28 09:53:28 +0530

    use random values and normalize values

--- stats.go
+++ stats.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	"math"
+	"math/rand"
 	"os"
+	"sort"
 	"strconv"
 
 	"github.com/mahesh-yadav/stats-app/logger"
 )
+
+func normalize(data []float64, mean float64, stdDev float64) []float64 {
+	if stdDev == 0 {
+		return data
+	}
+
+	normalized := make([]float64, len(data))
+
+	for i, val := range data {
+		normalized[i] = math.Floor((val-mean)/stdDev*10000) / 10000
+	}
+	return normalized
+}
+
+func randomFloat(min, max float64) float64 {
+	return min + rand.Float64()*(max-min)
+}
 
 func main() {
 
@@ -18,49 +37,43 @@ func main() {
 		logger.Fatal("Need one or more command-line arguments")
 	}
 
-	var min, max float64
-	initialized := false
-	nValues := 0
-	var sum float64
-	validNumbers := make([]float64, 0)
+	values := make([]float64, 0)
 
 	for i := 1; i < len(arguments); i++ {
 		num, err := strconv.ParseFloat(arguments[i], 64)
 		if err != nil {
 			continue
 		}
-		nValues = nValues + 1
-		validNumbers = append(validNumbers, num)
+		values = append(values, num)
+	}
+
+	if len(values) == 0 {
+		logger.Println("Generating 10 random values...")
+		for i := 0; i < 10; i++ {
+			values = append(values, randomFloat(-10, 10))
+		}
+	}
+	sort.Float64s(values)
+
+	logger.Println("Number of values: ", len(values))
+	logger.Println("Min: ", values[0])
+	logger.Println("Max: ", values[len(values)-1])
+
+	sum := float64(0)
+	for _, num := range values {
 		sum = sum + num
-		if !initialized {
-			min = num
-			max = num
-			initialized = true
-			continue
-		}
-		if num < min {
-			min = num
-		}
-		if num > max {
-			max = num
-		}
 	}
 
-	logger.Println("Number of values: ", nValues)
-	logger.Println("Min: ", min)
-	logger.Println("Max: ", max)
-
-	if nValues == 0 {
-		logger.Println("No valid numbers provided")
-		return
-	}
-	meanValue := sum / float64(nValues)
+	meanValue := sum / float64(len(values))
 	logger.Printf("Mean: %.5f\n", meanValue)
 
 	var squaredSum float64
-	for _, num := range validNumbers {
+	for _, num := range values {
 		squaredSum = squaredSum + math.Pow((num-meanValue), 2)
 	}
-	standardDeviation := math.Sqrt(squaredSum / float64(nValues))
+	standardDeviation := math.Sqrt(squaredSum / float64(len(values)))
 	logger.Printf("Standard Deviation: %.5f\n", standardDeviation)
+
+	normalized := normalize(values, meanValue, standardDeviation)
+	logger.Println("Normalized Values: ", normalized)
 }
